3aec4e017f531c30d6e79b4bf375dc0971637e7b
xiangshouding <fansekey@gmail.com>
2013-11-27 11:47:17 +0800

    new post

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@ jekyll build
 
 cd ../mysite
 
+# push output
 git add .
 git commit -m 'new post' -a
 git push origin master
 
 cd -
+
+# push source
+git add .
+git commit -m 'new post' -a
+git push origin master
